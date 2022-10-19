3049c4a5761535aa682a14d80c6d2e5b52e6f7d1
DeveloPolaroid <miguelttech.19@gmail.com>
2022-10-19 10:11:36 -0500

    Update RHEL-Config.sh

--- RHEL-Config.sh
+++ RHEL-Config.sh
@@ -1,41 +1,11 @@
 # Initial installation 
 sudo yum update -y
 sudo yum upgrade -y
-sudo yum install 
-filezilla \
-meld \
-youtube-dl \
-rofi \ 
-ranger \ 
-git \ 
-bat \ 
-vim \ 
-zsh \ 
-python3 \
-python3-pip \
-neofetch \ 
-ansible \ 
-unzip \
-nmap \
-xclip \
-maim \
-finch \
-lxappearance-devel \
-picom feh-3.8-2.fc36 \ 
-nitrogen \
-httpd \
-openssh \
-samba \
-firewalld -y
+sudo yum install filezilla meld youtube-dl rofi ranger git bat vim zsh python3 python3-pip neofetch ansible unzip nmap xclip maim finch lxappearance-devel picom feh-3.8-2.fc36 nitrogen httpd openssh samba firewalld -y
 
-pip3 install \
-awscli
-pip install \
---user bumblebee-status
-sudo dnf install \
-powerline-fonts \
-fontawesome-fonts \
-fira-code-fonts
+pip3 install awscli
+pip install --user bumblebee-status
+sudo dnf install powerline-fonts fontawesome-fonts fira-code-fonts
 
 
 # Customize Rofi
