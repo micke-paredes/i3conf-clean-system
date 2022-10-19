3fb9c0365280d2804d6b7b7c98e1a38a996ae728
DeveloPolaroid <miguelttech.19@gmail.com>
2022-10-19 10:26:51 -0500

    Update RHEL-Config.sh

--- RHEL-Config.sh
+++ RHEL-Config.sh
@@ -2,9 +2,9 @@
 sudo yum update -y
 sudo yum upgrade -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo yum install filezilla meld youtube-dl ffmpeg rofi ranger git bat vim zsh python3 python3-pip neofetch ansible unzip nmap xclip maim finch lxappearance-devel picom feh-3.8-2.fc36 nitrogen httpd openssh samba firewalld -y
+sudo yum install filezilla meld youtube-dl ffmpeg rofi ranger git bat vim zsh python3 python3-pip neofetch ansible unzip nmap xclip maim finch lxappearance-devel picom feh-3.8-2.fc36 nitrogen httpd openssh samba firewalld python3-netifaces -y
 
-pip3 install awscli psutil
+pip3 install awscli psutil netifaces
 pip install --user bumblebee-status
 sudo dnf install powerline-fonts fontawesome-fonts fira-code-fonts
 
