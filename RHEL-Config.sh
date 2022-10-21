bb0de9c611cb49ac6ffa0aa6140ddbe3bd5bd323
DeveloPolaroid <miguelttech.19@gmail.com>
2022-10-21 18:10:38 -0500

    Update RHEL-Config.sh

--- RHEL-Config.sh
+++ RHEL-Config.sh
@@ -35,10 +35,27 @@ sudo fc-cache -v
 
 # Setting up i3
 cd ~/Documents
+# i3 Lock
 git clone https://github.com/meskarune/i3lock-fancy/tree/dualmonitors
 cd i3lock-fancy/
 sudo make install
 cd ../
+# i3 alternate Layouts
 git clone https://github.com/olemartinorg/i3-alternating-layout
 sudo mv i3-alternating-layout/alternating_layouts.py /bin 
+# i3 gaps
+sudo yum install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \ 
+libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \ 
+libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
 rm -rf *
