1276ad73307f580c479ba5ac1da435c59c6558bc
DeveloPolaroid <miguelttech.19@gmail.com>
2022-10-21 18:21:42 -0500

    Update RHEL-Config.sh

--- RHEL-Config.sh
+++ RHEL-Config.sh
@@ -47,14 +47,5 @@ sudo yum install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
 libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \ 
 libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \ 
 libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd i3-gaps
-git checkout gaps && git pull
-autoreconf --force --install
-rm -rf build
-mkdir build
-cd build
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-make
-sudo make install
+sudo yum install i3-gaps-4.19.1-3.fc36 --allowerasing
 rm -rf *
